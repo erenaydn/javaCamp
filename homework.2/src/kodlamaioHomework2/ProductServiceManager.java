3a29673596eb309a9e3f39a913d7c6abd113a065
Eren Aydın <83093241+erenaydn@users.noreply.github.com>
2021-08-14 23:46:17 +0300

    up

--- homework.2/src/kodlamaioHomework2/ProductServiceManager.java
+++ homework.2/src/kodlamaioHomework2/ProductServiceManager.java
@@ -5,24 +5,28 @@ public class ProductServiceManager  {
 	public void add(Product product) {
 
 		System.out.println(product.getCourseName() + " adlý kurs eklendi. ");
+		System.out.println("-----");
 	}
 
 	public void delete(Product product) {
 
 		System.out.println(product.getCourseName() + " adlý kurs silindi. ");
+		System.out.println("-----");
 	}
 
 	public void update(Product product) {
 
 		System.out.println(product.getCourseName() + " adlý kurs güncellendi. ");
+		System.out.println("-----");
 	}
 	public void getAll(Product[] products) {
 		
 		for(Product product: products) {
 			
-			System.out.println(product.getCourseName());
-			System.out.println(product.getCourseInstructor());
-			System.out.println(product.getCourseDiscountedPrice());
+			System.out.println("Kurs adý : "+product.getCourseName());
+			System.out.println("Kurs hocasý : "+product.getCourseInstructor());
+			System.out.println("Kurs indirimli fiyatý : "+product.getCourseDiscountedPrice());
+			System.out.println("----- ");
 		}
 	}
 }
